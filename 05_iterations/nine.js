41db07875b6b2c36e4065a9a60613ff5eb7debbb
anshitraina <98584179+anshitraina@users.noreply.github.com>
2024-02-29 01:10:32 +0530

    nine.js  reduce function

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,1 +1,35 @@
-  
+  // reduce
+
+
+  const myNums= [1,2,3]
+
+//   const myTotal = myNums.reduce(function (accumulator, currentvalue) {
+//     console.log(`accumulator: ${accumulator} and Current-Value: ${currentvalue} `);
+//     return accumulator+currentvalue
+//   },0)
+//   console.log(myTotal);
+
+
+// using arrow function
+
+// const myTotal = myNums.reduce((accumulator,currentvalue)=>{
+//     console.log(`accumulator: ${accumulator} and current-Value: ${currentvalue}`); return accumulator+currentvalue},0)
+// console.log(myTotal);
+const shoppingCart = [
+    {
+        itemName: "Python",
+        price: 999
+    },
+    {
+        itemName: "Java",
+        price: 1999
+    },
+    {
+        itemName: "Cpp",
+        price: 1399
+    },
+]
+
+const billToPay =  shoppingCart.reduce((accumulator,item)=>{return accumulator+item.price
+},0)
+console.log(billToPay);
