c4d2b6dcae53488978e3ffa43bf151346730f9da
anshitraina <98584179+anshitraina@users.noreply.github.com>
2024-02-29 00:48:41 +0530

    seven.js

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -2,7 +2,7 @@ const myNumers = [1,2,3,4,5,6,7,8,9,10]
 
 // const newNums = myNumers.map( (num) => {return num+10})
 
-const newNums = myNumers
+const newNums = myNumers                       //chanining
                 .map((nums)=> {return nums*10})
                 .map((nums)=>{return nums+1})
                 .filter((nums)=> {return nums>=40})
